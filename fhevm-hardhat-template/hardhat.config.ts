4a803e7e90b1a060673e0051ef854a2553ae0416
evil001 <xiaojuntang80@gmail.com>
2025-10-12 23:48:56 +0800

    sol

--- fhevm-hardhat-template/hardhat.config.ts
+++ fhevm-hardhat-template/hardhat.config.ts
@@ -32,7 +32,7 @@ const config: HardhatUserConfig = {
     hardhat: {
       accounts: [
         {
-          privateKey: "0x0fafa549c1cf3bde8d44b79e56e231d4f048634823fbffc49b232798ec5c434a",
+          privateKey: "",
           balance: "100000000000000000"
         }
       ],
