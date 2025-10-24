c5f332f273e7aa96bb38fabae654597eb33607d2
evil001 <xiaojuntang80@gmail.com>
2025-10-24 23:09:29 +0800

    test deploy code

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-// 创建 axios 实例（避免污染全局 axios）
+// 创建 axios 实例（避免污染全局 axios-t）
 const request = axios.create({
     baseURL: 'https://privacy-prediction-market-fwqf-nopbhl4ex-hellessys-projects.vercel.app/api', // 后端接口基础地址
     timeout: 10000, // 超时时间
